f12f2f50bfec5f4f38c56493e517e7d7e12ab71e
sie2020a <64729917+sie2020a@users.noreply.github.com>
2026-01-10 16:20:00 +0900

    Update main.js

--- app1/main.js
+++ app1/main.js
@@ -1,1 +1,14 @@
+const btn = document.getElementById("btn");
+const input = document.getElementById("inputText");
+const output = document.getElementById("output");
 
+btn.addEventListener("click", () => {
+  const text = input.value.trim();
+
+  if (text === "") {
+    output.textContent = "何か入力してね";
+    return;
+  }
+
+  output.textContent = "入力された文字：" + text;
+});
