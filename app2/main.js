3cef052e05ab0874332d1dafb0b1ccf8336731e6
sie2020a <64729917+sie2020a@users.noreply.github.com>
2026-01-10 13:17:26 +0900

    Update main.js

--- app2/main.js
+++ app2/main.js
@@ -1,1 +1,7 @@
+const btn=document.getElementById("btn");
+const input=document.getElementById("input");
+const result=document.getElementById("result");
 
+btn.addEventListener("click",()=>{
+  result.textContent=input.value;
+});
