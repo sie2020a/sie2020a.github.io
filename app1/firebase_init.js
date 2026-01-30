750839bf84d9b1fb05427704289765dc40c58824
sie2020a <64729917+sie2020a@users.noreply.github.com>
2026-01-30 21:59:31 +0900

    Update firebase_init.js

--- app1/firebase_init.js
+++ app1/firebase_init.js
@@ -3,19 +3,19 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
 
-// ▼ここをFirebaseコンソールの値に置き換え
 const firebaseConfig = {
   apiKey: "AIzaSyD6uY03dlo9wPQBRHolvJ2C7orjRdjtqYo",
   authDomain: "schedule-app-410f9.firebaseapp.com",
   projectId: "schedule-app-410f9",
-  storageBucket: "schedule-app-410f9.appspot.com",
+  storageBucket: "schedule-app-410f9.firebasestorage.app",
   messagingSenderId: "645147677497",
   appId: "1:645147677497:web:2a19c74c40fac828764ee8",
   measurementId: "G-BBJ8VE29TH"
 };
+
 const app = initializeApp(firebaseConfig);
 
-// script.js がここから取る
+// 他のJSがここから取る
 window.firebaseApp = app;
 window.firebaseAuth = getAuth(app);
 window.firebaseDb = getFirestore(app);
