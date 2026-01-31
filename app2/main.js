c60b8e5bff9c3b8a239ecee323be0e183bddf939
sie2020a <64729917+sie2020a@users.noreply.github.com>
2026-01-31 09:57:19 +0900

    Delete app2/main.js

--- app2/main.js
+++
@@ -1,7 +0,0 @@
-const btn=document.getElementById("btn");
-const input=document.getElementById("input");
-const result=document.getElementById("result");
-
-btn.addEventListener("click",()=>{
-  result.textContent=input.value;
-});
