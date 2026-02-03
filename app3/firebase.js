ac6e942e4f7585e03729a025e0b6c06010dc728a
sie2020a <64729917+sie2020a@users.noreply.github.com>
2026-02-03 18:04:40 +0900

    Create firebase.js

---
+++ app3/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+const app = initializeApp(firebaseConfig);
+
+if (typeof window !== 'undefined') {
+  getAnalytics(app);
+}
+
+export default app;
