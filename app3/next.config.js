48756d98aa479b41a14bc049ba583f85727b381c
sie2020a <64729917+sie2020a@users.noreply.github.com>
2026-02-06 21:13:21 +0900

    Update next.config.js

--- app3/next.config.js
+++ app3/next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
+  trailingSlash: true,
+
+  // GitHub Pagesでサブフォルダ配信するため（超重要）
+  basePath: "/app3_site",
+  assetPrefix: "/app3_site/",
+
+  // 静的書き出し
+  output: "export",
+
+  // exportだと画像最適化が面倒なのでOFF
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
