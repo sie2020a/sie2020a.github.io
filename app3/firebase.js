9e47b52802ec2627a6134294e986cf1e5d265c00
sie2020a <64729917+sie2020a@users.noreply.github.com>
2026-02-06 20:46:14 +0900

    Update firebase.js

--- app3/firebase.js
+++ app3/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "ここ",
+  apiKey: "AIzaSyAEmWECKWJbjQyby8jRH-RJvm2371VQxSw",
   authDomain: "diary-32180.firebaseapp.com",
   projectId: "diary-32180",
   storageBucket: "diary-32180.appspot.com",
-  messagingSenderId: "ここ",
-  appId: "ここ"
+  messagingSenderId: "G-Q4KDTMX2CL"
+  appId: "1:975066690993:web:ad026fb50c3928089fb990",
 };
 
 const app = initializeApp(firebaseConfig);
