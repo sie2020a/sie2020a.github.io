172d3f9f4ec0eac7db78dfed91aa73bd5c4def5b
sie2020a <64729917+sie2020a@users.noreply.github.com>
2026-02-07 22:04:15 +0900

    Update firebase.js

--- app3/firebase.js
+++ app3/firebase.js
@@ -5,9 +5,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyAEmWECKWJbjQyby8jRH-RJvm2371VQxSw",
   authDomain: "diary-32180.firebaseapp.com",
   projectId: "diary-32180",
-  storageBucket: "diary-32180.appspot.com",
-  messagingSenderId: "G-Q4KDTMX2CL"
+  storageBucket: "diary-32180.firebasestorage.app",
+  messagingSenderId: "975066690993",
   appId: "1:975066690993:web:ad026fb50c3928089fb990",
+  measurementId: "G-Q4KDTMX2CL"
 };
 
 const app = initializeApp(firebaseConfig);
